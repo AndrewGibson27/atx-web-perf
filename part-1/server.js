262484dc6bdfa0bec518958eb1ab660d51cc84c8
Torre <tostaylo1@gmail.com>
2019-11-28 10:56:06 -0600

    Enabling latency for all static resource requests

--- part-1/server.js
+++ part-1/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const expressStaticGzip = require('express-static-gzip');
 const app = express();
 const shrinkRay = require('shrink-ray-current');
 
-app.use(shrinkRay());
-app.use(
-	'/',
-	expressStaticGzip('public/', {
-		enableBrotli: true,
-		orderPreference: ['br']
-	})
-);
+// Order matters for the app.use statements
+app.use(shrinkRay()); // Comment out this line to disable BR encoding
+
+// Comment out this code block to disable delay in server response
+app.use(function(req, res, next) {
+	setTimeout(next, 1000);
+});
+
+app.use(express.static('public'));
 
 const listener = app.listen('3000', function() {
 	console.log('Your app is listening on port 3000 ');
